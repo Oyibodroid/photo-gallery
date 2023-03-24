9d54c71514bd3800dcbd7e325d4938eb7a404854
Oyibo Daniel <oyibodaniel247@gmail.com>
2023-03-23 17:01:33 -1200

    13th commit

--- nav.js
+++ nav.js
@@ -4,17 +4,27 @@ function myFunction() {
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
+
+
+function burger(){
+    document.getElementById("burger").classList.toggle("open");
 }
 
 
+
+function myForm() {
+    document.getElementById("drop-form").classList.toggle("drop");
+}
+
+// window.onclick = function (event) {
+//     if (!event.target.matches('#burger')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
